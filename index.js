044359542c45717edcc36a7a953f53804fde685e
han30230 <sanghoonhan@naver.com>
2023-02-13 15:52:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var chatHistory = [];
 $("#chat-submit").click(function() {
   var message = $("#chat-input").val();
-  $("#chat-output").append("<p>" + message + "</p>");
+  $("#chat-output").append("<p>You: " + message + "</p>");
+  chatHistory.push("You: " + message);
   $("#chat-input").val("");
+});
+$("#chat-history").click(function() {
+  alert(chatHistory.join("\n"));
 });

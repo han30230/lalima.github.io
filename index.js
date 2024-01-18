aff482e007b49f84606637e131a013e8e4baf583
han30230 <sanghoonhan@naver.com>
2024-01-18 09:53:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var chatHistory = [];
-$("#chat-submit").click(function() {
-  var message = $("#chat-input").val();
-  $("#chat-output").append("<p>You: " + message + "</p>");
-  chatHistory.push("You: " + message);
-  $("#chat-input").val("");
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu ul');
+
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('hide');
 });
-$("#chat-history").click(function() {
-  alert(chatHistory.join("\n"));
-});
+
+// var chatHistory = [];
+// $("#chat-submit").click(function() {
+//   var message = $("#chat-input").val();
+//   $("#chat-output").append("<p>You: " + message + "</p>");
+//   chatHistory.push("You: " + message);
+//   $("#chat-input").val("");
+// });
+// $("#chat-history").click(function() {
+//   alert(chatHistory.join("\n"));
+// });

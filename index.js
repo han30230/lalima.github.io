4551fd46a71e97728e9a8e8cf14f51cb1626c6ab
han30230 <sanghoonhan@naver.com>
2024-01-18 10:04:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu ul');
+const navItems = document.querySelectorAll(".nav-item");
 
-menuIcon.addEventListener('click', () => {
-    menu.classList.toggle('hide');
+navItems.forEach((navItem, i) => {
+  navItem.addEventListener("click", () => {
+    navItems.forEach((item, j) => {
+      item.className = "nav-item";
+    });
+    navItem.className = "nav-item active";
+  });
 });
 
-// var chatHistory = [];
-// $("#chat-submit").click(function() {
-//   var message = $("#chat-input").val();
-//   $("#chat-output").append("<p>You: " + message + "</p>");
-//   chatHistory.push("You: " + message);
-//   $("#chat-input").val("");
+
+// const menuIcon = document.querySelector('.menu-icon');
+// const menu = document.querySelector('.menu ul');
+
+// menuIcon.addEventListener('click', () => {
+//     menu.classList.toggle('hide');
 // });
-// $("#chat-history").click(function() {
-//   alert(chatHistory.join("\n"));
-// });
+
+// // var chatHistory = [];
+// // $("#chat-submit").click(function() {
+// //   var message = $("#chat-input").val();
+// //   $("#chat-output").append("<p>You: " + message + "</p>");
+// //   chatHistory.push("You: " + message);
+// //   $("#chat-input").val("");
+// // });
+// // $("#chat-history").click(function() {
+// //   alert(chatHistory.join("\n"));
+// // });
